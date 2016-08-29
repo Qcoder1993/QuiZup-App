f6c8c20021bd7859c081f5c605ba92139f96cb1b
ashish703 <apachertr10@gmail.com>
2016-08-29 23:40:56 +0530

    commit by ashish
    Signed-off-by: ashish703 <apachertr10@gmail.com>

---
+++ app/src/main/java/com/apachertr10gmail/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.apachertr10gmail.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // TODO: Your application init goes here.
+                Intent mInHome = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 600);
+    }
+
+
+}
